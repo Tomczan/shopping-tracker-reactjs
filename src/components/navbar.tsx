6ab06de5ee71fd0d28f126f4b7c6abbb82a1ab23
Tomczan <t.czuba95@gmail.com>
2023-04-14 22:56:56 +0200

    add: links to home and login pages

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,11 +1,17 @@
+import { Link } from "react-router-dom"
+
 export const Navbar = () => {
   return (
     <div className="flex flex-row">
       <div className="basis-1/4 bg-blue-500">01</div>
       <div className="basis-1/2 bg-green-500">02</div>
       <div className="basis-1/4 bg-blue-600 flex flex-row">
-        <p className="basis-1/4">1</p>
-        <p className="basis-1/4">2</p>
+        <p className="basis-1/4">
+          <Link to="/">Home</Link>
+        </p>
+        <p className="basis-1/4">
+          <Link to="/login/">Login</Link>
+        </p>
         <p className="basis-1/4">3</p>
         <p className="basis-1/4">4</p>
       </div>
