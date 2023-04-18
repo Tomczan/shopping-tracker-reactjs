c9c332e1df78da6b51b27bd241103f43ea3c7e68
Tomczan <t.czuba95@gmail.com>
2023-04-18 21:57:35 +0200

    Add AuthProvider which deals with login from login form WIP

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,11 +1,53 @@
 import { createContext, useState, useEffect } from "react"
 
 type AuthContextType = {
-  isAuthenticated: boolean
   username: string | null
+  authToken: string | null
+  loginUser: (e: React.FormEvent<HTMLFormElement>) => Promise<void>
 }
 
-export const AuthContext = createContext<AuthContextType>({
-  isAuthenticated: false,
-  username: null,
-})
+export const AuthContext = createContext<AuthContextType | null>(null)
+
+type AuthProviderProps = {
+  children: React.ReactNode
+}
+
+export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
+  const [storedUsername, setUsername] = useState<string | null>(null)
+  const [storedAuthToken, setAuthToken] = useState<string | null>(null)
+
+  const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const login = e.currentTarget.elements.namedItem(
+      "username"
+    ) as HTMLInputElement
+    const password = e.currentTarget.elements.namedItem(
+      "password"
+    ) as HTMLInputElement
+    await fetchUser(login.value, password.value)
+  }
+
+  const fetchUser = async (login: string, password: string) => {
+    let response = await fetch("http://127.0.0.1:8000/api/token/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username: login, password: password }),
+    })
+    let data = await response.json()
+    console.log(data)
+  }
+
+  let contextData = {
+    username: storedUsername,
+    authToken: storedAuthToken,
+    loginUser: handleLogin,
+  }
+
+  return (
+    <AuthContext.Provider value={{ ...contextData }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
