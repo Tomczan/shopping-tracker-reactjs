3b7a89e7184c4571ab7ad10b0ab15bdf7ea10d89
Tomczan <t.czuba95@gmail.com>
2023-04-23 17:38:31 +0200

    Add AuthContext in PrivateRoute to check is user is logged in

--- src/utils/PrivateRoute.tsx
+++ src/utils/PrivateRoute.tsx
@@ -1,11 +1,15 @@
+import { useContext } from "react"
 import { Navigate } from "react-router-dom"
+import { AuthContext } from "../context/AuthContext"
 
 export type PrivateRouteProps = {
   children: JSX.Element
 }
 
 const PrivateRoute = ({ children }: PrivateRouteProps) => {
-  const isAuthenticated = true
+  let authContext = useContext(AuthContext)
+  const isAuthenticated = authContext?.authToken ? true : false
+  console.log(isAuthenticated)
 
   if (!isAuthenticated) {
     return <Navigate to={"/login"} />
