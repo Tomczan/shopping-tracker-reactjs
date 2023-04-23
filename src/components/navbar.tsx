479e028ffdd8af3ace95fe24d6a2921f6207923e
Tomczan <t.czuba95@gmail.com>
2023-04-23 17:34:51 +0200

    Add AuthContext in navbar to check if user is logged in

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,6 +1,10 @@
+import { useContext } from "react"
 import { Link } from "react-router-dom"
+import { AuthContext } from "../context/AuthContext"
 
 const Navbar = () => {
+  let authContext = useContext(AuthContext)
+
   return (
     <div className="flex flex-row">
       <div className="basis-1/4 bg-blue-500">01</div>
@@ -11,6 +15,11 @@ const Navbar = () => {
         </p>
         <p className="basis-1/4">
           <Link to="/login/">Login</Link>
+          {authContext?.authToken ? (
+            <span>Zalogowany</span>
+          ) : (
+            <span>Niezalogowany</span>
+          )}
         </p>
         <p className="basis-1/4">3</p>
         <p className="basis-1/4">4</p>
