cb6047c6d42e5eac4aba8ce15aef8e5ec2fae28f
Tomczan <t.czuba95@gmail.com>
2023-05-03 22:22:10 +0200

    CSS

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -6,24 +6,59 @@ const Navbar = () => {
   let authContext = useContext(AuthContext)
 
   return (
-    <div className="flex flex-row bg-blue-500 shadow-md text-white w-screen h-8 items-center">
-      <div className="basis-1/4 ml-4 ">
-        <Link to="/">Home</Link>
+    <nav className="bg-blue-500">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between h-16">
+          <div className="flex items-center">
+            <Link
+              to="/"
+              className="text-white font-medium tracking-wide text-2xl"
+            >
+              Home
+            </Link>
+          </div>
+          <div className="flex items-center">
+            <Link
+              to="/option2"
+              className="text-white hover:bg-blue-400 px-3 py-2 rounded-md text-sm font-medium"
+            >
+              Option 2
+            </Link>
+          </div>
+          <div className="flex items-center">
+            <div className="flex items-center">
+              <Link
+                to="/page1"
+                className="text-white hover:bg-blue-400 px-3 py-2 rounded-md text-sm font-medium"
+              >
+                Page 1
+              </Link>
+              <Link
+                to="/page2"
+                className="text-white hover:bg-blue-400 px-3 py-2 rounded-md text-sm font-medium"
+              >
+                Page 2
+              </Link>
+              {authContext?.authToken ? (
+                <button
+                  onClick={authContext.logoutUser}
+                  className="text-white hover:bg-blue-400 px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Logout
+                </button>
+              ) : (
+                <Link
+                  to="/login"
+                  className="text-white hover:bg-blue-400 px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Login
+                </Link>
+              )}
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="basis-1/2 ">Option 2</div>
-      <div className="basis-1/4 flex flex-row">
-        <p className="basis-1/4">1</p>
-        <div className="basis-1/4">2</div>
-        <p className="basis-1/4">3</p>
-        <p className="basis-1/4 mr-4">
-          {authContext?.authToken ? (
-            <p onClick={authContext.logoutUser}>Logout</p>
-          ) : (
-            <Link to="/login/">Login</Link>
-          )}
-        </p>
-      </div>
-    </div>
+    </nav>
   )
 }
 
