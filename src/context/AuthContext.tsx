aabad65cfc6cc3e486690a8047b52d3cb482bf5f
Tomczan <t.czuba95@gmail.com>
2023-05-03 21:40:04 +0200

    Refactor

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,6 +1,8 @@
 import { createContext, useState, useEffect } from "react"
 import jwt_decode from "jwt-decode"
 import { useNavigate } from "react-router-dom"
+import axiosInstance from "../utils/axiosInstance"
+import axios from "axios"
 
 export type AuthTokensType = {
   refresh: string
@@ -10,17 +12,22 @@ export type AuthTokensType = {
 type AuthContextType = {
   username: string | null
   authToken: AuthTokensType | null
-  loginUser: (e: React.FormEvent<HTMLFormElement>) => Promise<void>
-  logoutUser: () => void
+  isAuthenticated: boolean
+  loginUser: (e: React.FormEvent<HTMLFormElement>) => Promise<void> | null
+  logoutUser: () => void | null
 }
 
-type AuthProviderProps = {
-  children: React.ReactNode
-}
+export const AuthContext = createContext<AuthContextType>({
+  username: null,
+  authToken: null,
+  isAuthenticated: false,
+  loginUser: () => null,
+  logoutUser: () => null,
+})
 
-export const AuthContext = createContext<AuthContextType | null>(null)
-
-export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
+export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
   const navigate = useNavigate()
 
   const getDecodedUsername = (accessToken: string): string => {
@@ -39,6 +46,7 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
   const [storedAuthToken, setAuthToken] = useState<AuthTokensType | null>(
     () => parsedLocalStorageTokens
   )
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false)
 
   const loginUser = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
@@ -52,23 +60,25 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
   }
 
   const fetchUser = async (login: string, password: string) => {
-    let response = await fetch("http://127.0.0.1:8000/api/token/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ username: login, password: password }),
-    })
-
-    if (response.status === 200) {
-      let data = await response.json()
-      setAuthToken(data)
-      setUsername(getDecodedUsername(data.access))
-      localStorage.setItem("authTokens", JSON.stringify(data))
-      navigate(-1)
-    } else {
-      alert("Something went wrong!")
-    }
+    await axiosInstance
+      .post("api/token/", {
+        username: login,
+        password: password,
+      })
+      .then((response) => {
+        console.log(response)
+        if (response.status === 200) {
+          let data = response.data
+          setAuthToken(data)
+          setUsername(getDecodedUsername(data.access))
+          localStorage.setItem("authTokens", JSON.stringify(data))
+          setIsAuthenticated(true)
+          navigate(-1)
+        }
+      })
+      .catch((error) => {
+        console.log(`error: ${error}`)
+      })
   }
 
   const logoutUser = () => {
@@ -103,6 +113,7 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
     authToken: storedAuthToken,
     loginUser: loginUser,
     logoutUser: logoutUser,
+    isAuthenticated: isAuthenticated,
   }
 
   // useEffect(() => {
