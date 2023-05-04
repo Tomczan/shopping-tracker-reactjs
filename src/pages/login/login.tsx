b1895db4e4329d777ed336d7a129c016a589b1c7
Tomczan <t.czuba95@gmail.com>
2023-05-04 17:50:26 +0200

    Install shadcn ui

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,5 +1,7 @@
 import { useContext, useState } from "react"
 import { AuthContext } from "../../context/AuthContext"
+import { Button } from "../../components/ui/button"
+import { Input } from "../../components/ui/input"
 
 const Login = () => {
   let authContext = useContext(AuthContext)
@@ -13,7 +15,7 @@ const Login = () => {
       >
         <input type="text" name="username" placeholder="username" />
         <input type="password" name="password" placeholder="password" />
-        <input type="submit" />
+        <Button type="submit">Login</Button>
       </form>
     </div>
   )
