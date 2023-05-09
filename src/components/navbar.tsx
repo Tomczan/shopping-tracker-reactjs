505a101f1dde80047035c89d494d1f8f03258c8e
Tomczan <t.czuba95@gmail.com>
2023-05-09 21:00:28 +0200

    Style progress, right menu with blurred background on small screens

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,74 +4,59 @@ import { AuthContext } from "../context/AuthContext"
 import { MenuIcon, X as CloseIcon } from "lucide-react"
 
 const Navbar = () => {
+  // document.body.classList.add("dark")
   let authContext = useContext(AuthContext)
 
   return (
-    <nav className="border-b border-slate-900/10 bg-slate-100 bg-opacity-60 dark:border-slate-400 dark:bg-slate-900">
-      <div className="mx-auto mb-3 max-w-7xl pt-4 sm:px-6">
-        <div className="ml-4 flex h-8 justify-between">
-          <div className=" flex items-center">
-            <Link
-              to="/"
-              className="text-3xl font-medium tracking-wide transition hover:text-blue-500"
+    <nav className="mb-3 border-b border-slate-900/10 bg-slate-100 bg-opacity-60 px-6 pt-4 ">
+      <div className="flex items-center justify-between">
+        <div className="">
+          <Link
+            to="/"
+            className="text-3xl font-medium tracking-wide transition hover:text-blue-500"
+          >
+            Home
+          </Link>
+        </div>
+
+        {/* <ul className=" gap-8 transition"> */}
+        <ul className="inset fixed inset-0 ml-32 flex flex-col gap-8 bg-black bg-opacity-10 p-6 pt-24 backdrop-blur-sm transition md:static md:ml-0 md:flex md:flex-row md:bg-inherit md:p-0 md:pt-0 md:backdrop-blur-0">
+          <li>
+            <a href="#" className="text-lg hover:text-cyan-500 md:my-0">
+              Option 1
+            </a>
+          </li>
+          <li>
+            <a href="#" className="text-lg hover:text-cyan-500 md:my-0">
+              Option 2
+            </a>
+          </li>
+          <li>
+            <a href="#" className="text-lg hover:text-cyan-500 md:my-0">
+              Option 3
+            </a>
+          </li>
+          <li>
+            <a href="#" className="text-lg hover:text-cyan-500 md:my-0">
+              Option 4
+            </a>
+          </li>
+        </ul>
+
+        <div className="flex">
+          {authContext?.authToken ? (
+            <button
+              onClick={authContext.logoutUser}
+              className="rounded-md  hover:bg-blue-400"
             >
-              Home
+              Logout
+            </button>
+          ) : (
+            <Link to="/login" className="rounded-md  hover:bg-blue-400">
+              Logout
             </Link>
-          </div>
-
-          <ul className="mx-4 hidden items-center transition md:flex">
-            <li>
-              <a
-                href="#"
-                className="mx-2 text-lg duration-500 hover:text-cyan-500 md:my-0"
-              >
-                Option 1
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="mx-2  bg-transparent  p-0 text-lg transition hover:text-blue-500"
-              >
-                Option 2
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="mx-2  bg-transparent  p-0 text-lg transition hover:text-blue-500"
-              >
-                Option 3
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="mx-2 bg-transparent p-0 text-lg transition hover:text-blue-500"
-              >
-                Option 4
-              </a>
-            </li>
-          </ul>
-
-          <div className="mr-4 flex w-12 items-center">
-            {authContext?.authToken ? (
-              <button
-                onClick={authContext.logoutUser}
-                className="rounded-md px-3 py-2 text-sm font-medium hover:bg-blue-400"
-              >
-                Logout
-              </button>
-            ) : (
-              <Link
-                to="/login"
-                className="rounded-md px-3 text-sm font-medium hover:bg-blue-400"
-              >
-                Logout
-              </Link>
-            )}
-            <MenuIcon className="md:hidden" />
-          </div>
+          )}
+          <MenuIcon />
         </div>
       </div>
     </nav>
