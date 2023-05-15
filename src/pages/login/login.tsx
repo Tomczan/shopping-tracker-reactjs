20da8d500e1c1becf317bae1bc7c870d0a11ec72
Tomczan <t.czuba95@gmail.com>
2023-05-15 21:21:57 +0200

    Refactor login page

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -5,16 +5,29 @@ import { Input } from "../../components/ui/input"
 
 const Login = () => {
   let authContext = useContext(AuthContext)
+  const [login, setLogin] = useState<string>("")
+  const [password, setPassword] = useState<string>("")
 
   return (
     <div>
       <form
         onSubmit={(e) => {
-          authContext!.loginUser(e)
+          e.preventDefault()
+          authContext!.loginUser(login, password)
         }}
       >
-        <input type="text" name="username" placeholder="username" />
-        <input type="password" name="password" placeholder="password" />
+        <input
+          type="text"
+          name="username"
+          placeholder="username"
+          onChange={(event) => setLogin(event.target.value)}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          onChange={(event) => setPassword(event.target.value)}
+        />
         <Button type="submit">Login</Button>
       </form>
     </div>
