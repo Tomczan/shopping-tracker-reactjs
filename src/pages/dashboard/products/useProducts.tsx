2f59e6dd146fe62d365268749cf467bdf6a67708
Tomczan <t.czuba95@gmail.com>
2023-06-07 20:54:20 +0200

    Add products component with custom hook

---
+++ src/pages/dashboard/products/useProducts.tsx
@@ -0,0 +1,46 @@
+import useAuthAxios from "../../../hooks/useAuthAxios"
+
+export interface IProduct {
+  id: number
+  created: string
+  discount_price: string
+  finished: null | string
+  opened: null | string
+  price: string
+  product: {
+    id: number
+    brand: string
+    name: string
+  }
+  shop: {
+    id: number
+    name: string
+  }
+  updated: string
+}
+
+const useProducts = () => {
+  const api = useAuthAxios()
+
+  const getUserProducts = (): Promise<IProduct[]> => {
+    return api
+      .get("api/my-products/")
+      .then((response) => {
+        if (response.status === 200) {
+          return response.data as IProduct[]
+        } else {
+          throw new Error("Failed to fetch user's products.")
+        }
+      })
+      .catch((error) => {
+        console.error(error)
+        throw error
+      })
+  }
+
+  return {
+    getUserProducts,
+  }
+}
+
+export default useProducts
