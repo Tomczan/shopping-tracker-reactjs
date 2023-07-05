6f60e23803b6cd36b92c474bda645f6f0ae9ac57
Tomczan <t.czuba95@gmail.com>
2023-07-05 18:50:15 +0200

    Basic usage of getProductDetail method from useProduct hook

--- src/pages/dashboard/product/product.tsx
+++ src/pages/dashboard/product/product.tsx
@@ -1,8 +1,39 @@
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import useProduct, { IProduct } from "../useProduct"
 
 const Product = () => {
   const { id } = useParams()
-  return <div>Product details o id: {id}</div>
+  const { getProductDetail } = useProduct()
+  const [products, setProducts] = useState<IProduct[]>([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getProductDetail(id!)
+        setProducts(data)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    fetchData()
+  }, [])
+
+  return (
+    <div>
+      {" "}
+      {products.map((item) => (
+        <div
+          className="h-56 w-64 rounded-lg border bg-slate-400 p-8 shadow-sm"
+          key={item.id}
+        >
+          <span> Nazwa: {item.product.name}</span>
+          <br />
+          <span> Cena: {item.price}</span>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default Product
